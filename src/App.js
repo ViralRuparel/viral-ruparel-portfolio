ca27abe25cd9f28d16511b00978e81d7158407df
Viral Ruparel <viral@sysbioz.com>
2020-02-08 21:32:56 +0530

    landing page design done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import ScrollMagic from 'scrollmagic';
+import viral from './viral.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App horizontal-scroll-wrapper squares">
-      <div className="contentDivs">Landing Page</div>
-      <div className="contentDivs">About</div>
-      <div className="contentDivs">Skills</div>
-      <div className="contentDivs">Work</div>
-      <div className="contentDivs">Experience</div>
-      <div className="contentDivs">Contact</div>
-      <div id="path">PATHWAY</div>
-    </div >
+    <div>
+      <div id="scrollParent">
+        <div className="App horizontal-scroll-wrapper squares">
+          <div id="landingPage" className="contentDivs">
+            <div>
+              <img className="image" src={viral}></img>
+            </div>
+            <div>
+              <div className="titleDiv">
+                <span className="viralRuparel">Viral Ruparel</span>
+                <span className="andHis">and his</span>
+                <span className="andHis">connected dots</span>
+              </div>
+            </div>
+          </div>
+          <div className="contentDivs">About</div>
+        </div >
+      </div>
+      <div id="path"><span id="char"></span></div>
+    </div>
+
   );
 }
 
 export default App;
+
+{/* 
+<div className="contentDivs">Skills</div>
+<div className="contentDivs">Work</div>
+<div className="contentDivs">Experience</div>
+<div className="contentDivs">Contact</div> */}
