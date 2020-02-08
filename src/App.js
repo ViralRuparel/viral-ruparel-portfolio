a840238ee09dd41daff438682f86f89d223ea304
Viral Ruparel <viral@sysbioz.com>
2020-02-08 23:40:22 +0530

    magicwand added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ScrollMagic from 'scrollmagic';
+import Anime, { anime } from 'react-anime';
 import viral from './viral.png'
 import './App.css';
 
@@ -9,22 +10,24 @@ function App() {
       <div id="scrollParent">
         <div className="App horizontal-scroll-wrapper squares">
           <div id="landingPage" className="contentDivs">
-            <div>
-              <img className="image" src={viral}></img>
-            </div>
-            <div>
-              <div className="titleDiv">
-                <span className="viralRuparel">Viral Ruparel</span>
-                <span className="andHis">and his</span>
-                <span className="andHis">connected dots</span>
+            <Anime opacity={[0, 1]} delay={(e, i) => i * 1000} scale={[.5, .9]}>
+              <div>
+                <img className="image" src={viral}></img>
               </div>
-            </div>
+              <div>
+                <div className="titleDiv">
+                  <span className="viralRuparel">Viral Ruparel</span>
+                  <span className="andHis">and his</span>
+                  <span className="andHis">connected dots</span>
+                </div>
+              </div>
+            </Anime>
           </div>
           <div className="contentDivs">About</div>
         </div >
-      </div>
+      </div >
       <div id="path"><span id="char"></span></div>
-    </div>
+    </div >
 
   );
 }
