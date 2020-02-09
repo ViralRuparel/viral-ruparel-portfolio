3ba511c8c45250e24596b9584fcd5a2ec6b6ad38
Viral Ruparel <viral@sysbioz.com>
2020-02-09 07:39:17 +0530

    contact done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import Anime, { anime } from 'react-anime';
 import viral from './viral.png'
 import hogwartsExpress from './hogwartsExpress.png'
 import magicWardPoint from './magicWardPoint.png'
+import flyingCar from './flyingCar.png'
+import watson from './watson.png'
+import university from './university.png'
+import envelope from './envelope.png'
+import viralruparel_resume from './viralruparel_resume.pdf'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <i id="resumeLink" class="fas fa-file"> <a href={viralruparel_resume} target="_blank">Resume</a></i>
       <div id="scrollParent">
         <div className="App horizontal-scroll-wrapper squares">
           <div id="landingPage" className="contentDivs">
@@ -55,6 +61,61 @@ function App() {
             </p>
             </div>
           </div>
+          <div id="skillsDiv" className="contentDiv">
+            <div id="flyingCar">
+              <img src={flyingCar}></img>
+            </div>
+            <div id="skillsHeading">
+              Part 2: <br />Skills
+            </div>
+          </div>
+          <div id="skillsDetails" className="contentDiv">
+
+            <div>
+              <div>
+                <p><i className="devicon-javascript-plain colored" /><span>Javascript</span></p>
+                <p><i className="devicon-react-original colored" /><span>React</span></p>
+                <p><i className="devicon-nodejs-plain colored" /><span>NodeJS</span></p>
+                <p><i className="devicon-mongodb-plain colored" /><span>MongoDB</span></p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <p><i className="devicon-swift-plain colored" /><span>Swift</span></p>
+                <p><i className="devicon-css3-plain colored" /><span>CSS3</span></p>
+                <p><i className="devicon-html5-plain colored" /><span>HTML5</span></p>
+                <p><img src={watson} className="watsonImage" /><span>IBM Watson Assistant</span></p>
+              </div>
+            </div>
+
+          </div>
+          <div id="experienceDiv" className="contentDivs">
+            <div id="flyingCar">
+              <img src={university}></img>
+            </div>
+            <div id="skillsHeading">
+              Part 3: <br />Experience
+            </div>
+          </div>
+          <div id="contactDiv" className="contentDivs">
+            <div id="envelope">
+              <img src={envelope}></img>
+            </div>
+            <div id="contactHeading">
+              Part 4: <br />Contact
+            </div>
+          </div>
+          <div id="contactDetails" className="contentDivs">
+            <div>
+              <a href="https://www.linkedin.com/in/viral-ruparel-3b194297/" target="_blank"><i className="fab fa-linkedin-in" aria-hidden="true"></i></a>
+              <a href="https://github.com/ViralRuparel" target="_blank"><i class="fab fa-github"></i></a>
+              <a href="https://medium.com/@viralruparel" target="_blank"><i class="fab fa-medium-m"></i></a>
+              <a href="https://twitter.com/ViralRuparel" target="_blank"><i class="fab fa-twitter"></i></a>
+              <a href="https://www.facebook.com/viral.ruparel" target="_blank"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://www.instagram.com/thatponderingmind/" target="_blank"><i class="fab fa-instagram"></i></a>
+              <a href="mailto:viralruparel@gmail.com" target="_blank"><i class="fas fa-at"></i></a>
+            </div>
+          </div >
         </div >
       </div >
       <div id="path"><span id="char"></span></div>
@@ -65,8 +126,13 @@ function App() {
 
 export default App;
 
-{/* 
-<div className="contentDivs">Skills</div>
+{/*
+  <a href="https://www.linkedin.com/feed/" target="_blank"><i className="fab fa-linkedin-in" aria-hidden="true"></i></a>
+              <a href="https://www.linkedin.com/feed/" target="_blank"></a><i class="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/feed/" target="_blank"></a><i class="fab fa-medium-m"></i></a>
+          <a href="https://www.linkedin.com/feed/" target="_blank"></a><i class="fab fa-twitter"></i></a>
+        <a href="https://www.linkedin.com/feed/" target="_blank"></a><i class="fab fa-facebook-f"></i></a> 
+<i class="devicon-github-plain"></i>
 <div className="contentDivs">Work</div>
-<div className="contentDivs">Experience</div>
+<i className="fab fa-linkedin-in" aria-hidden="true"></i>
 <div className="contentDivs">Contact</div> */}
